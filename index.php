8c71e9dcd70b2874e7d4900cf35adfe8beef95f6
killer <quaylynrimer11@gmail.com>
2022-02-13 14:19:48 -0700

    added index. php and resources to display tested with php -S localhost:8000

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html ml-update="aware" lang="en"><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Album example · Bootstrap v5.0</title>
+
+
+    
+
+    <!-- Bootstrap core CSS -->
+    <link href="./resources/bootstrap-5.0.2-dist/css/bootstrap.css" rel="stylesheet">
+
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+  </head>
+  <body>
+    
+      <?php 
+        require_once("./bootstrap5_hero_snipets.php") ; 
+        require_once("./bootstrap5_footer_snipets.php") ; 
+
+      ?>
+
+
+
+
+    <script src="./resources/bootstrap-5.0.2-dist/js/bootstrap.bundle.js" ></script>
+
+      
+  
+
+</body></html>
